71a290cbde27bfbe05a0aa60e230d15a08dd300f
Avinash Kumar <avinashkumar245@gmail.com>
2021-05-16 15:17:39 +0530

    Initial Commit Till Combobox control

---
+++ Seleinum/Seleinum/CustomControl.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Seleinum
+{
+    public  class CustomControl:DriverHelper
+    {
+        public static void ComboBox(string controlName,string value )
+        {
+            IWebElement combobox = driver.FindElement(By.XPath($"//input[@id='{controlName}-awed']"));
+            combobox.Clear();
+            combobox.SendKeys("Mango");
+
+            driver.FindElement(By.XPath($"//div[@id='{controlName}-dropmenu']//li[text()='{value}']")).Click();
+        }
+    }
+}
---
+++ Seleinum/Seleinum/UnitTest1.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Seleinum
+{
+    public class Tests: DriverHelper
+    {
+        
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+           
+            driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+            driver.Manage().Window.Maximize();
+            IWebElement element=driver.FindElement(By.Id("ContentPlaceHolder1_Meal"));
+            element.SendKeys("Tomato");
+
+            IWebElement celery = driver.FindElement(By.XPath("//*[@name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Celery']"));
+            celery.Click();
+
+            string comboboxxControlname = "ContentPlaceHolder1_AllMealsCombo";
+
+            
+            CustomControl.ComboBox(comboboxxControlname, "Mango");
+
+            //IWebElement combobox = driver.FindElement(By.XPath("//input[@id='ContentPlaceHolder1_AllMealsCombo-awed']"));
+            //combobox.Clear();
+            //combobox.SendKeys("Mango");
+
+            //driver.FindElement(By.XPath("//div[@id='ContentPlaceHolder1_AllMealsCombo-dropmenu']//li[text()='Mango']")).Click();
+
+
+            Assert.Pass();
+        }
+    }
+}
